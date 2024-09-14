981df4eecd0d7f37ff435fab784d393df017bc72
AppleFlavored <boostelixir@gmail.com>
2024-09-14 00:36:05 -0400

    build: update polar to 1.11.2

--- worlds/build.gradle.kts
+++ worlds/build.gradle.kts
@@ -18,6 +18,6 @@ dependencies {
     compileOnly(project(":sdk"))
     compileOnly("dev.flavored:bamboo:1.1.0")
     compileOnly("net.minestom:minestom-snapshots:1f34e60ea6")
-    compileOnly("dev.hollowcube:polar:1.11.1")
+    compileOnly("dev.hollowcube:polar:1.11.2")
     compileOnly("cc.ekblad:4koma:1.2.0")
 }
