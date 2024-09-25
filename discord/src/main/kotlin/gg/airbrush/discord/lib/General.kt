e86c7e6f867c403fed10343ab1b3b759ac70df84
prettyflowerss <me@astrid.sh>
2024-09-25 11:10:42 -0400

    feat: player list command

--- discord/src/main/kotlin/gg/airbrush/discord/lib/General.kt
+++ discord/src/main/kotlin/gg/airbrush/discord/lib/General.kt
@@ -1,7 +1,7 @@
 /*
  * This file is part of Airbrush
  *
- * Copyright (c) 2023 Airbrush Team
+ * Copyright (c) Airbrush Team
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -10,31 +10,15 @@
  * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-package gg.airbrush.discord.discordCommands
+package gg.airbrush.discord.lib
 
-import gg.airbrush.discord.lib.answer
-import gg.airbrush.sdk.SDK
-import me.santio.coffee.common.annotations.Command
-import me.santio.coffee.jda.annotations.Description
+import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
-import java.util.*
 
-@Command
-@Description("Unlink your Minecraft account")
-class UnlinkCommand {
-	fun main(e: SlashCommandInteractionEvent) {
-		e.deferReply(true).queue()
-
-		val playerData = SDK.players.getByDiscordID(e.user.id)
-		if(playerData == null) {
-			e.answer("Your account is not currently linked!")
-			return
-		}
-
-		val sdkPlayer = SDK.players.get(UUID.fromString(playerData.uuid))
-		sdkPlayer.wipeDiscordId()
-
-		e.answer("Successfully unlinked your Discord account")
-	}
+fun SlashCommandInteractionEvent.answer(msg: String) {
+    return this.hook.sendMessage(msg).queue()
 }
 
+fun SlashCommandInteractionEvent.answer(msg: MessageEmbed) {
+    return this.hook.sendMessageEmbeds(msg).queue()
+}
